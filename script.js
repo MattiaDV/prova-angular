fbea18d83ff68cbfafa6e6e133e116e2710db42b
MattiaDV <mattiadv25105@gmail.com>
2025-03-29 17:19:42 +0100

    a

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 var app = angular.module("myApp", ['ngSanitize']);
+// Stringa
 app.controller("myController", function($scope) {
     $scope.message = "Hello World!";
 });
 
+// Stringa
 app.controller("myControllerDue", function($scope) {
     $scope.welcome = "Ciao bro";
 })
 
+// Div
 app.controller("myControllerTre", function($scope) {
     $scope.card = "<div class = 'aaa'>prova</div>";
 })
